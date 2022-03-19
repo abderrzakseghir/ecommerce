b0a774b9b4578a36467f32ed553f10c70b25ceb5
asegh <a.seghir@esi-sba.dz>
2022-03-19 17:53:24 +0100

    sdfgn

--- lib/Controller/data/respotries/auth_repo_impl.dart
+++ lib/Controller/data/respotries/auth_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/Controller/domain/respotries/respo_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepoImpl extends AuthRepo {
   @override
@@ -23,12 +24,12 @@ class AuthRepoImpl extends AuthRepo {
         await googleUser?.authentication;
 
     // Create a new credential
-    /* final credential = GoogleAuthProvider.credential(
+    final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
-    );*/
+    );
 
     // Once signed in, return the UserCredential
-    // return await FirebaseAuth.instance.signInWithCredential(credential);
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
